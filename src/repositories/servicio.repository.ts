3d70771125aa9cf59c907a9ce4c4a174c9d96a22
GloriaF27 <gloriaa.foreron@ecci.edu.co>
2021-11-06 10:33:59 -0500

    first commit

---
+++ src/repositories/servicio.repository.ts
@@ -0,0 +1,31 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {Servicio, ServicioRelations, Encomienda, Cliente} from '../models';
+import {EncomiendaRepository} from './encomienda.repository';
+import {ClienteRepository} from './cliente.repository';
+
+export class ServicioRepository extends DefaultCrudRepository<
+  Servicio,
+  typeof Servicio.prototype.id,
+  ServicioRelations
+> {
+
+  public readonly encomiendafk: BelongsToAccessor<Encomienda, typeof Servicio.prototype.id>;
+
+  public readonly origenfk: BelongsToAccessor<Cliente, typeof Servicio.prototype.id>;
+
+  public readonly destinofk: BelongsToAccessor<Cliente, typeof Servicio.prototype.id>;
+
+  constructor(
+    @inject('datasources.Mongo') dataSource: MongoDataSource, @repository.getter('EncomiendaRepository') protected encomiendaRepositoryGetter: Getter<EncomiendaRepository>, @repository.getter('ClienteRepository') protected clienteRepositoryGetter: Getter<ClienteRepository>,
+  ) {
+    super(Servicio, dataSource);
+    this.destinofk = this.createBelongsToAccessorFor('destinofk', clienteRepositoryGetter,);
+    this.registerInclusionResolver('destinofk', this.destinofk.inclusionResolver);
+    this.origenfk = this.createBelongsToAccessorFor('origenfk', clienteRepositoryGetter,);
+    this.registerInclusionResolver('origenfk', this.origenfk.inclusionResolver);
+    this.encomiendafk = this.createBelongsToAccessorFor('encomiendafk', encomiendaRepositoryGetter,);
+    this.registerInclusionResolver('encomiendafk', this.encomiendafk.inclusionResolver);
+  }
+}
